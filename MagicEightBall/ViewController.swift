35c3d0aae832938b9c1febba1a67698b080bff5e
Tasha Jamison <jamisont@vt.edu>
2015-11-03 15:12:17 -0800

    Initial Commit

---
+++ MagicEightBall/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MagicEightBall
+//
+//  Created by Tatiana Jamison on 11/3/15.
+//  Copyright © 2015 jamisont. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
